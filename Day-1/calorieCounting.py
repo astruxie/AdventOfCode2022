14857c6363b70b0fd975cb07dc38fa74324e5f49
Samantha Noggle <44234583+astruxie@users.noreply.github.com>
2022-12-01 17:09:46 -0500

    all done for the day

--- Day-1/calorieCounting.py
+++ Day-1/calorieCounting.py
@@ -1,6 +1,9 @@
 elves = []
+
 f = open("input.txt", "r")
+
 tempCals = 0
+
 for line in f:
     if line == "\n":
         elves.append(tempCals)
@@ -8,3 +11,15 @@ for line in f:
     else:
         tempCals += int(line)
 print(max(elves))
+
+# part two
+
+topThreeTotal = 0
+
+for _ in range(3):
+    topThreeTotal += max(elves)
+    elves.pop(elves.index(max(elves)))
+
+
+print(topThreeTotal)
+
